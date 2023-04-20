d7089a4a5c5c9329d1cdd13e6246f7986c7b06bf
Larsouille <superthithi2008@gmail.com>
2023-04-20 21:07:43 +0200

    some little improvements

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::io;
+use std::io::{self, Write};
 use std::net::TcpStream;
 
+
+/// It reads a line and remove \r and \n to the returned string.
 fn read_line() -> String {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(_) => input.to_owned().replace("\r", "").replace("\n", ""),
+        Ok(_) => input.to_owned().replace(['\r', '\n'], ""),
         Err(err) => {
             panic!("ERR: {}", err);
         }
@@ -12,15 +14,19 @@ fn read_line() -> String {
 }
 
 fn main() {
-    let mut stream: TcpStream;
+    let mut stream: Option<TcpStream> = None;
 
     loop {
-        println!("📜  Menu :");
-        println!("1] 📡 Connect to server");
-        println!("2] 📨 Send a message");
-        println!("3] 👂 Listen for response of the server");
-        println!("4] ❌ Disconnect to server");
-        println!("5] 👋 Exit");
+        print!("  eztcp | ");
+        match stream {
+            Some(ref s) => print!("Connected to {}", s.peer_addr().unwrap()),
+            None => print!("Not connected")
+        }
+        println!("1]  Connect to server");
+        println!("2]  Send a message");
+        println!("3]  Listen for response of the server");
+        println!("4]  Disconnect to server");
+        println!("5]  Exit");
 
         let input = if let Ok(i) = read_line().parse::<i32>() {
             i
@@ -30,22 +36,33 @@ fn main() {
 
         match input {
             1 => {
+                print!("IP address to connect to: ");
+                io::stdout().flush().unwrap();
                 let addr = read_line();
 
                 match TcpStream::connect(addr) {
                     Ok(s) => {
-                        stream = s;
-                        println!(" 📡 Connect successfully to `{:?}`!", stream);
+                        stream = Some(s);
+                        println!(" 📡 Connect successfully to `{}`!", stream.unwrap().peer_addr().unwrap());
                     }
                     Err(err) => {
                         println!("ERR: {}", err);
                     }
                 }
             }
+            2 => {
+                // let msg = read_line();
+                // match stream.write(msg.as_bytes()) {
+                //     Ok(a) => {
+                //         println!("a = {a}");
+                //     }
+                //     Err(err) => println!("ERR: {}", err)
+                // }
+            }
             5 => break,
             _ => println!(" Enter a correct number between 1 and 5."),
         }
 
-        println!(""); // It's here because it looks prettier with it
+        println!(); // It's here because it looks prettier with it
     }
 }
